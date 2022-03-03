5ef56c31584d70f7c365c7f7f6f4a67ba50aa032
Arsalan-Aslam <arsalanaslam89@gmail.com>
2022-03-03 20:08:26 +0500

    Added firebase firestore Database with realtime update (onSnapshot)

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,6 +1,17 @@
-import React, {useState} from 'react';
-import '../Todos/Todos.css'
-
+import React, {useEffect, useState} from 'react';
+import '../Todos/Todos.css';
+import {db} from '../../config/firebase';
+import {
+    collection,
+    addDoc,
+    getDocs,
+    doc,
+    updateDoc,
+    deleteDoc,
+    onSnapshot,
+    query,
+    where
+} from 'firebase/firestore';
 
 function Todos () {
 
@@ -8,31 +19,118 @@ function Todos () {
     const [inputValue, setInputValue] = useState("");
     const [updatedValue, setUpdatedValue] = useState("");
     const [indexValue, setIndexValue] = useState("");
+    // const [refresh, setRefresh] = useState(false);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // useEffect(async () => {
+    //     const dbRef = collection(db, "todos");
+    //     const data = await getDocs(dbRef);
+    //     // console.log(data);
+    //     let getTodo = [];
+    //     data.forEach((doc) => {
+    //         getTodo.push({
+    //             key: doc.id, 
+    //             todo: doc.data().todo
+    //         });
+    //     });
+    //     setTodos(getTodo);
+    // }, [refresh]);
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(async () => {
+        const dbRef = collection(db, "todos");
+        const q = query(dbRef);
+        const dataFeed = onSnapshot(q, (querySnapshot) => {
+            let getTodo = [];
+            querySnapshot.forEach((doc) => {
+                getTodo.push({key:doc.id, todo:doc.data().todo});
+            });
+            setTodos(getTodo);
+        });
+    }, []);
+
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
+    // useEffect(() => {
+    //     db.collection('todos').onSnapshot(snapshot => (
+    //         setTodos(snapshot.docs.map(doc => doc.data()))
+    //     ))
+    // }, []);
+
+    // console.log(todos);
 
 
-const addToDo = (e) => {
+const addToDo = async (e) => {
     e.preventDefault();
     if(inputValue === "") {
         alert("Please enter a task")
     } else {
         // console.log(inputValue);
-        todos.push(inputValue);
-        setTodos([...todos]);
-        setInputValue("");
+        // todos.push(inputValue);
+        // setTodos([...todos]);
+        try {
+            const dbRef = collection(db, "todos");
+            const addData = await addDoc(dbRef,{todo:inputValue});
+            // console.log(addData);
+            setInputValue("");
+            // setRefresh(!refresh);
+        } catch (error) {
+            console.log(error)
+        }
     } 
     
 } 
 
-const updateToDo = (index) => {
+// const updateToDo = (index) => {
+//     if(updatedValue === "") {
+//         alert("Please enter an updated details");
+//     } else {
+//         // console.log(inputValue);
+//         todos.splice(index,1,updatedValue);
+//         setTodos([...todos]);
+//         setUpdatedValue("");
+//         setIndexValue("");
+//         console.log(todos);
+//     }     
+// }
+
+// const updateToDo = (index) => {
+//     if(updatedValue === "") {
+//         alert("Please enter an updated details");
+//     } else {
+//         console.log(inputValue);
+//         todos.splice(index,1,updatedValue);
+//         setTodos([...todos]);
+//         setUpdatedValue("");
+//         setIndexValue("");
+//         console.log(todos);
+        
+//     }     
+// }
+
+
+const updateToDo = async (key) => {
     if(updatedValue === "") {
         alert("Please enter an updated details");
     } else {
         // console.log(inputValue);
-        todos.splice(index,1,updatedValue);
-        setTodos([...todos]);
-        setUpdatedValue("");
-        setIndexValue("");
-        console.log(todos);
+        // todos.splice(index,1,updatedValue);
+        // setTodos([...todos]);
+        // setUpdatedValue("");
+        // setIndexValue("");
+        // console.log(todos);
+        // console.log(key);
+        const dbRef = doc(db, "todos", key);
+        const obj = {todo: updatedValue};
+
+        try{
+            const updateTodo = await updateDoc(dbRef, obj);
+            // console.log(updateTodo);
+            // setRefresh(!refresh);
+            setUpdatedValue("");
+            setIndexValue("");
+        } catch (error) {
+            console.log(error);
+        }
     }     
 }
 
@@ -40,17 +138,31 @@ const cancelTodo = () => {
     setIndexValue("");
 }
 
-const deleteTodo = (index) => {
-    console.log(index);
-    // console.log("todo", todos.filter((v, i) => i !== index));
-    // todos.filter((v, i) => i !== index)
-    todos.splice(index,1);
-    setTodos([...todos]);
-    console.log(todos); 
+// const deleteTodo = (index) => {
+//     console.log(index);
+//     // console.log("todo", todos.filter((v, i) => i !== index));
+//     // todos.filter((v, i) => i !== index)
+//     todos.splice(index,1);
+//     setTodos([...todos]);
+//     console.log(todos); 
+// } 
+
+const deleteTodo = async(key) => {
+    // console.log(key);
+    const dbRef = doc(db, "todos", key);
+    await deleteDoc(dbRef);
+    // setRefresh(!refresh); 
 } 
 
-const deleteAll = () => {
-    setTodos([]);
+// const deleteAll = () => {
+//     setTodos([]);
+// }
+
+const deleteAll = async () => {
+    await todos.forEach((todo)=>{
+        deleteDoc(doc(db, "todos", todo.key));
+        // setRefresh(!refresh);
+    });
 }
 
 
@@ -70,15 +182,15 @@ const deleteAll = () => {
                         return indexValue === i? (  
                     
                         <div key={i} className="update-task">
-                            <input type="text"defaultValue={v} onChange={(e) => setUpdatedValue(e.target.value)} autoFocus />
-                            <button id="update" onClick={()=>updateToDo(i)}>Update</button> 
+                            <input type="text"defaultValue={v.todo} onChange={(e) => setUpdatedValue(e.target.value)} autoFocus />
+                            <button id="update" onClick={()=>updateToDo(v.key)}>Update</button> 
                             <button id="cancel" onClick={cancelTodo}>Cancel</button>
                         </div>
                         ) : (
                         <div key={i} className="task">
-                            <li>{v}</li>
+                            <li>{v.todo}</li>
                             <button id="edit" onClick={()=>setIndexValue(i)}>Edit</button> 
-                            <button id="delete" onClick={() =>deleteTodo(i)} >Delete</button>
+                            <button id="delete" onClick={()=>deleteTodo(v.key)} >Delete</button>
                         </div>
                         );
                         
